f860fceea1b3b041ecbde8a1df66d8c617227450
Bedoni Corentin <corentin.bedoni@imerir.com>
2018-04-18 14:46:23 +0200

    centrage

--- Serveur/algorithmes/utile.py
+++ Serveur/algorithmes/utile.py
@@ -3,11 +3,9 @@ def centrage(image1,image2):
 
 def distance(image1,image2):
 	x=len(image1)
-	y=len(image1[0])
 	retour = 0
 	for i in range (0,x):
-		for j in range(0,y):
-			if image1[i][j] != image2[i][j]:
-				retour+=1
+		if image1[i]!= image2[i]:
+			retour+=1
 	return retour;
 
