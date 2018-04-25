952d6a27e15b0120cf329789212ec583ba51eb8b
gcelli <guillaume.celli@imerir.com>
2018-04-25 11:14:03 +0200

    Add poids in database

--- Serveur/python-flask-server/swagger_server/database.py
+++ Serveur/python-flask-server/swagger_server/database.py
@@ -30,6 +30,7 @@ class Database:
         self.dataTrain = client.Reconnaissance_Chiffre.get_collection("DataTrain")
         self.confusionMatrix = client.Reconnaissance_Chiffre.get_collection("ConfusionMatrix")
         self.dataTest = client.Reconnaissance_Chiffre.get_collection("DataTest")
+        self.poids = client.Reconnaissance_Chiffre.get_collection("Poids")
 
     def insertDataTrain(self, data):
         self.dataTrain.insert(data)
@@ -41,7 +42,7 @@ class Database:
         result = self.confusionMatrix.find_one({"method": methodName})
         self.confusionMatrix.insert(
             {"method": methodName, "matrix": matrix}) if not result else self.confusionMatrix.find_and_modify(
-            {"method": methodName, "matrix": matrix})
+            {"method": methodName}, {"method": methodName, "matrix": matrix})
         return True
 
     def getMatrix(self, methodName):
@@ -89,6 +90,14 @@ class Database:
             for row in s:
                 self.dataTest.insert({"data": list(map(int, row[1:])), "solution": row[0]})
 
+    def savePoids(self, poids):
+        result = self.poids.find_one({"method": "reseau"})
+        self.poids.insert(
+            {"method": "reseau", "poids": poids}) if not result else self.poids.find_and_modify(
+            {"method": "reseau"}, {"method": "reseau", "poids": poids})
+        return True
 
-
+    def getPoids(self):
+        return self.poids.find_one()["poids"]
+        
 db = Database()
