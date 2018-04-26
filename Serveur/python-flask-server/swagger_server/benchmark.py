f20fd7aceba0b681548941ce6c1c4e2fa766b279
Bedoni Corentin <corentin.bedoni@imerir.com>
2018-04-26 14:59:34 +0200

    changement bayeseienne, largeur, longueur

--- Serveur/python-flask-server/swagger_server/benchmark.py
+++ Serveur/python-flask-server/swagger_server/benchmark.py
@@ -30,9 +30,7 @@ class Benchmark:
         trainBaye([swagger_server.algorithmes.utile.centrageSolo(t["data"], 6, 8) for t in trainData], [t["solution"] for t in trainData])
 
         resultB = findUsingBaye(swagger_server.algorithmes.utile.centrageSolo(image['data'], 6, 8),
-                                [-4, -17, 12, 14, 39, -13, 3, 19, 7, -13, -6, 23, 39, 21, 11, 1, 23, 15, -19, 36, 0, 29,
-                                -19, -5, 50, 11, 36, 7, 14, 3, -13, 34, 34, 20, -8, 6, 18, 15, 26, 13, 11, 4, 17, 9, 34,
-                                -4, 1, 2])
+                                [4.14, 46.99, 24.19, 15, 12.04, 9, 15.31, 18, 59.58, 20.99, 61.95, 66.41, 8, 12])
 
         n = NeuralNet(48, 10, 30, 2, 0.1)
         resultN = n.guess(swagger_server.algorithmes.utile.centrageSolo(image['data'],6, 8))
