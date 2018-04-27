008a481c8b87447ee47bad571c7bf13ffda207d5
valerian <vacier@kang.fr>
2018-04-27 14:07:43 +0200

    Comment in the code

--- Serveur/python-flask-server/swagger_server/algorithmes/kmeans.py
+++ Serveur/python-flask-server/swagger_server/algorithmes/kmeans.py
@@ -1,12 +1,18 @@
 from collections import Counter
 
 
-def findUsingKMeans(data, solutions, toFind, distanceFunction, k = 3):
+def findUsingKMeans(data, solutions, toFind, distanceFunction, k=3):
+    # Get the k nearest point
     nearestPoints = sorted(data, key=lambda r: distanceFunction(r, toFind))[:k]
+    # Return the majority in the nearest points
     return max([(k, v) for k, v in Counter([solutions[data.index(v)] for v in nearestPoints]).items()], key=lambda x: x[1])[0]
 
-def findMultipleUsingKMeans(data, solutions, toFinds, distanceFunction, k = 3):
-    return [findUsingKMeans(data, solutions, i, distanceFunction) for i in toFinds]
 
-def findImageUsingKMeans(data, solutions, toFind, distanceFunction, k = 3):
+def findMultipleUsingKMeans(data, solutions, toFinds, distanceFunction, k=3):
+    # K-means for multiples values
+    return [findUsingKMeans(data, solutions, i, distanceFunction, k) for i in toFinds]
+
+
+def findImageUsingKMeans(data, solutions, toFind, distanceFunction, k=3):
+    # Return the nearest point
     return sorted(data, key=lambda r: distanceFunction(r, toFind))[:k]
